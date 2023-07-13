da9435fbcbb1c128d41dcc2a4bd2be10ddee4184
Carlos Guirado <carlosguirado@users.noreply.github.com>
2023-07-13 17:37:50 +0000

    Updated EV_VMT_State_Spillover scripts
    Single script has been split into two, with step 3 taken out and run independently.

--- scripts/EV_VMT_state_spillover.py
+++ scripts/EV_VMT_state_spillover.py
@@ -33,8 +33,6 @@ ccst_lookup_file = 'ccst_geoid_key_tranps_geo_with_imputation.csv'
 hb_spillover_file = 'spillover/NHTS_home_based_trip_rate_spillover.csv'
 hb_border_frac_file = 'spillover/home_based_border_fraction.csv'
 
-# I NEED TO USE THIS LAST FILE
-
 # destination choice input
 dist_matrix_file = 'combined/distance_matrix_by_tracts_' + selected_state + '.csv'
 employment_file = 'wac_tract_2017.csv'
@@ -56,7 +54,8 @@ transit_by_tract = read_csv('Network/' + transit_file, sep = ',')
 dest_choice_param = read_csv('Input/' + dest_choice_file, sep = ',')
 
 # load list of route 
-# list_of_routes = os.listdir('Input/' + selected_state + '/route_external')
+#list_of_routes = os.listdir('Input/route/' + selected_state + '_external')
+
 # <codecell>
 
 ####################################################################################
@@ -351,8 +350,11 @@ transit_by_tract_short.columns = ['destination', 'with_transit']
 ## process O-D distance skims
 sample_dist_matrix = dist_matrix.loc[dist_matrix['origin'].isin(origin_tracts)]
 
-#sample_dist_matrix['destination'] = sample_dist_matrix['destination'].astype(int)
-sample_dist_matrix['destination'] = pd.to_numeric(sample_dist_matrix['destination'], downcast='integer')
+# Using .loc indexing to avoid SettingWithCopyWarning message in console
+#sample_dist_matrix.loc[:,'destination'] = sample_dist_matrix['destination'].astype(int)
+sample_dist_matrix.loc[:, 'destination'].astype(int)
+
+#sample_dist_matrix['destination'] = pd.to_numeric(sample_dist_matrix['destination'], downcast='integer')
 sample_dist_matrix = sample_dist_matrix.loc[sample_dist_matrix['distance'] <= max_radius]
 
 # # append destination attributes
@@ -455,6 +457,7 @@ trip_attraction_with_choice.loc[:,  'TripGeneration'] *= trip_attraction_with_ch
 trip_attraction_with_choice.loc[:,  'TripGeneration'] = np.round(trip_attraction_with_choice.loc[:,  'TripGeneration'], 0)
 trip_attraction_with_choice = \
      trip_attraction_with_choice.loc[trip_attraction_with_choice['TripGeneration'] > 0]
+print('Trip Attraction with Choice:')
 print(trip_attraction_with_choice.loc[:,  'TripGeneration'].sum())
 trip_attraction_with_choice["distance"].plot(kind="hist", density = True,
                                               weights = trip_attraction_with_choice["TripGeneration"], bins = 50)
@@ -470,96 +473,5 @@ output_var = ['GEOID', 'home_geotype', 'home_microtype', 'populationGroupType',
 trip_attraction_with_choice_out = trip_attraction_with_choice[output_var]
 
 trip_attraction_with_choice_out.to_csv('Output/' + selected_state + '/OD_home_based_trips_spillover.csv.zip')
-
+print('OD Home Based Trips Spillover CSV file created')
 # <codecell>
-
-####################################################################################
-############ step 3 -- route choice model with shortest-path, not cleaned ##########
-####################################################################################
-
-# route choice
-# od_trips = trip_attraction_with_choice_out.copy()
-# od_trips.loc[:, 'VMT'] = od_trips.loc[:, 'distance'] * od_trips.loc[:, 'TripGeneration']
-# print(od_trips.loc[:, 'TripGeneration'].sum())
-# print(od_trips.loc[:, 'VMT'].sum())
-# print(len(od_trips))
-# od_trips = od_trips.rename(columns = {'GEOID': 'home_GEOID', 'distance': 'gc_distance'})
-# # od_trips = od_trips.drop(columns=['distance'])
-# od_trips['destination'] = od_trips['destination'].astype(int)
-
-# od_trips['OD'] = od_trips['home_GEOID'].astype(str) + '_' + od_trips['destination'].astype(str)
-
-# ccst_lookup_short = ccst_lookup[['GEOID', 'geotype', 'microtype']]
-
-# # assign route to trips
-# meter_to_mile = 0.000621371
-
-# grouping_var = ['GEOID', 'geotype', 'microtype', 
-#                 'home_GEOID', 'home_geotype', 'home_microtype', 'populationGroupType'] # through tract + home attributes
-
-# grouping_var_2 = ['home_GEOID', 'home_geotype', 'home_microtype', 'populationGroupType', 'destination'] # through tract + home attributes
-
-# file_dir = 'Input/' + selected_state + '/route_external'
-# filelist = [file for file in os.listdir(file_dir) if (file.endswith('.csv'))]
-# route_df = pd.concat([read_csv(file_dir + '/' + f) for f in filelist ])
-
-# route_df = route_df.loc[route_df['Length'] > 0]
-# route_df = pd.merge(route_df, ccst_lookup_short, on = 'GEOID', how = 'left')
-# route_df['destination'] = route_df['destination'].astype(int)
-# # print(route_df.columns)
-# trip_to_route = pd.merge(od_trips, route_df,
-#                       left_on = ['home_GEOID', 'destination'],
-#                       right_on = ['source', 'destination'],
-#                       how = 'inner')
-# sample_origin_tract = trip_to_route['GEOID'].unique()[0]
-# # trip_to_route = trip_to_route.dropna()
-# trip_to_route.loc[:, 'distance'] *= meter_to_mile
-# trip_to_route.loc[:, 'VMT'] = trip_to_route.loc[:, 'Length'] * trip_to_route.loc[:, 'TripGeneration'] * \
-# meter_to_mile
-
-# trip_to_route.loc[:, 'VMT'] = np.round(trip_to_route.loc[:, 'VMT'], 0)
-# trip_to_route = trip_to_route.loc[trip_to_route['VMT'] > 0]
-
-# VMT_to_home = trip_to_route.groupby(grouping_var)[['VMT']].sum()
-# VMT_to_home = VMT_to_home.reset_index() 
-# # VMT_to_home['VMT'] = np.round(VMT_to_home['VMT'], 0)
-# # VMT_to_home = VMT_to_home[VMT_to_home['VMT'] > 0]
-
-# # VMT_to_home_out = pd.concat([VMT_to_home_out, VMT_to_home])
-    
-
-# VMT_to_destination = trip_to_route.groupby(grouping_var_2)[['VMT']].sum()
-# VMT_to_destination = VMT_to_destination.reset_index() 
-    
-# # VMT_to_destination_out = pd.concat([VMT_to_destination_out, VMT_to_destination])
-
-
-# OD_summary = trip_to_route.groupby(grouping_var_2)[['gc_distance', 'distance']].mean()
-# OD_summary = OD_summary.reset_index()
-    
-# OD_summary.loc[:, 'OD'] = OD_summary['home_GEOID'].astype(str) + '_' + OD_summary['destination'].astype(str)
-
-# # OD_summary_out = pd.concat([OD_summary_out, OD_summary])
-# unique_ODs = OD_summary.OD.unique()
-# od_trips = od_trips.loc[~ od_trips['OD'].isin(unique_ODs)] # remove trips with route assigned
-# print('total trip VMT = ' + str(VMT_to_home.loc[:, 'VMT'].sum()))  
-# # post-processing data
-
-# VMT_to_home.to_csv('Output/' + selected_state + '/home_daily_vmt_spillover.csv', index = False)
-# VMT_to_destination.to_csv('Output/' + selected_state + '/destination_daily_vmt_spillover.csv', index = False)
-# OD_summary.to_csv('Output/' + selected_state + '/OD_summary_spillover.csv', index = False)
-
-# # <codecell>
-# # find O-Ds that need impute
-# od_trips_impute = od_trips.groupby(['home_GEOID', 'destination', 'OD'])[['VMT']].sum()
-# od_trips_impute = od_trips_impute.reset_index()
-# print(od_trips_impute.VMT.sum())
-# od_trips_impute = od_trips_impute.drop_duplicates(subset = 'OD')
-# od_trips_impute = od_trips_impute[od_trips_impute['home_GEOID'] != od_trips_impute['destination']]
-# od_trips_impute = od_trips_impute.sort_values(by = 'VMT', ascending = False)
-# od_trips_impute.loc[:, 'fraction'] = od_trips_impute.loc[:, 'VMT'] / od_trips_impute.loc[:, 'VMT'].sum()
-# od_trips_impute.loc[:, 'fraction'] =od_trips_impute.loc[:, 'fraction'].cumsum()
-# od_trips_impute = od_trips_impute[od_trips_impute['fraction'] <= 0.95]
-# od_trips_impute = od_trips_impute[od_trips_impute['VMT'] > 50]
-# print(od_trips_impute.VMT.sum())
-# od_trips_impute.to_csv('Network/' + selected_state + '/OD_to_impute_spillover.csv')
